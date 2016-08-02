2bce93a4b5d1a42aa1033e05570b3f69f6875284
Amanda Olczak <aolczak@gmail.com>
2016-08-02 12:01:56 -0400

    got lyric appended to div, added background image

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-var ex = require('./lyrics.js');
+var lyr = require('./lyrics.js');
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/api/lyric', function(req, res) {
-  res.send('hello world');
+app.get('/api/lyrics', function(req, res) {
+  var selection = Math.floor(Math.random() * lyr.length);
+  res.send(lyr[selection]);
+  // res.send('lyr');
+});
+
+app.get('/', function(req, res) {
+  console.log("where am I?");
 });
 
 // app.listen(3000, function() {
--- public/script.js
+++ public/script.js
@@ -1,8 +1,17 @@
 //script.js, any js file that might perform AJAX operations
+// var lyric = '';
 var lyric = '';
 
-$.get('/api/lyric', function(data) {
+$.get('/api/lyrics', function(data) {
   lyric = data;
+  var textInsert = document.getElementById("quote");
+  var textNode = document.createTextNode(lyric);
+  textInsert.appendChild(textNode);
   console.log(lyric);
 });
 //logic to display lyric on the page
+
+// app.get('/', function(req, res) {
+//   var selection = Math.floor(Math.random() * lyr.length);
+//   res.send(lyr[selection]);
+// });
